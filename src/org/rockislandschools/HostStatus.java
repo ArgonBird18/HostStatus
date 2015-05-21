69339e6bdd23488bec63fbc302f6ef89f5216b33
Michael MacKenna <mpmackenna@gmail.com>
2015-05-21 10:46:11 -0500

    Added JFrame and JPanel

--- src/org/rockislandschools/HostStatus.java
+++ src/org/rockislandschools/HostStatus.java
@@ -4,6 +4,21 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 public class HostStatus {
+    
+    public String IsReachableReturnString(String ip){
+        String canBeReachedReturnString = "Down";
+        int timeout = 10000;
+        
+        
+        
+        try {
+            if (InetAddress.getByName(ip).isReachable(timeout)) canBeReachedReturnString = "Up";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+                
+        return canBeReachedReturnString;
+    }
 
     public boolean IsReachable(String ip){
 
@@ -14,7 +29,7 @@ public class HostStatus {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(canBeReached);
+        //System.out.println(canBeReached);
         return canBeReached;
     }
 
--- src/org/rockislandschools/Main.java
+++ src/org/rockislandschools/Main.java
@@ -1,5 +1,14 @@
 package org.rockislandschools;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +18,35 @@ public class Main {
             System.out.println("Please specify IP Address.");
             System.exit(0);
         }
-
+        
         String ipAdd = args[0];
-
+        
         HostStatus state = new HostStatus();
         state.IsReachable(ipAdd);
-
+        
+        JFrame frame = new JFrame("Host Status");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+        
+        JPanel panel = new JPanel();
+        
+        JLabel ipLabel = new JLabel(ipAdd);
+        ipLabel.setOpaque(true);
+        ipLabel.setBackground(Color.white);
+        ipLabel.setPreferredSize(new Dimension(20, 25));
+        
+        JLabel stateLabel = new JLabel(state.IsReachableReturnString(ipAdd));
+        stateLabel.setOpaque(true);
+        stateLabel.setBackground(Color.yellow);
+        stateLabel.setPreferredSize(new Dimension(20, 25));
+        
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel.setPreferredSize(new Dimension(400,400));
+        frame.getContentPane().add(panel);
+        frame.getContentPane().add(ipLabel, BorderLayout.CENTER);
+        frame.getContentPane().add(stateLabel, BorderLayout.AFTER_LAST_LINE);
+        frame.pack();
+        frame.setVisible(true);
+  
     }
 }
